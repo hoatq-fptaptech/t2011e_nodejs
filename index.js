0b8da1e56bfe4d3221bfc5b59182ff1bd3c5759c
billytrinh <quanghoa.trinh@gmail.com>
2021-03-23 21:05:41 +0700

    up code nodejs

--- index.js
+++ index.js
@@ -1,24 +1,67 @@
-const express = require("express");// goi module express de su dung
-const app = express(); // xay nha - tao dich vu host
-const port = process.env.PORT || 5000; // bao rang dich vu se su dung port 5000
+const express = require("express");
+const app = express();
 
-// tao web
-app.listen(port,function () {
-    console.log("Server running...");
+// start hosting nodejs port 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,function () {
+    console.log("server is running....");
 });
-app.use(express.static("public")); // cho phep cac file tinh (css, img, js frontend)
-// tim 1 nhan vien de hoi cai minh can -- routing
-
-// tao routing cho trang chu
+// ket noi db de lam viec voi du lieu
+const mssql = require("mssql");
+const config = {
+    server:"118.70.125.210",
+    user:"sa",
+    password:"z@GH7ytQ",
+    database:"QuangHoa"
+};
+mssql.connect(config,function (err) {
+    if(err) console.log(err);
+    else console.log("ket noi DB thanh cong!");
+});
+// tao 1 bien de lam viec voi db
+const sql = new mssql.Request();
+// tao 1 routing
 app.get("/",function (req,res) {
-    res.send("<h1>Xin chao cac ban</h1>");
+    res.send("xin chao");
 });
-
-// them 1 routing moi
-app.get("/bong-da",function (req,res) {
-    res.sendFile(__dirname+"/public/demo_layout.html");
-})
-// __dirname -> tim ra duong dan tuyet doi den ma nguon trong may chu
-app.get("/bong-da/ronaldo",function (req,res) {
-    res.sendFile(__dirname+"/public/demo_layout.html");
-})
+// khai bao web se dung view engine la ejs
+app.set("view engine","ejs");
+// cap quyen truy cap cac file static trong public
+app.use(express.static("public"));
+// tao 1 routing chuyen dua ra danh sach khach hang
+app.get("/danh-sach-khach-hang",function (req,res) {
+   var ds = [];
+   var txt_sql = "select * from KhachHang";
+   sql.query(txt_sql,function (err,rows) {
+        if(err) ds = ["Khong co khach hang nao ca"];
+        else ds = rows.recordset;
+        res.render("danhsachkhachhang",{
+            ds:ds
+        });
+   });
+   // res.send(ds);
+});
+// tao 1 routing chuyen dua ra danh sach hang hoa
+app.get("/danh-sach-hang-hoa",function (req,res) {
+    var ds = [];
+    var txt_sql = "select * from HangHoa";
+    sql.query(txt_sql,function (err,rows) {
+        if(err) ds = ["Khong co hang hoa nao ca"];
+        else ds = rows.recordset;
+        res.send(ds);
+    });
+    // res.send(ds);
+});
+// tao 1 routing chuyen dua ra danh sach hang hoa
+app.get("/tim-kiem-hang-hoa",function (req,res) {
+    var thamsoxyz = req.query.tentimkiem;
+    var ds = [];
+    var txt_sql = "select * from HangHoa where Ten like N'%"+
+        thamsoxyz+"%' OR MoTa like N'%"+thamsoxyz+"%'";
+    sql.query(txt_sql,function (err,rows) {
+        if(err) ds = ["Khong co hang hoa nao ca"];
+        else ds = rows.recordset;
+        res.send(ds);
+    });
+    // res.send(ds);
+});
